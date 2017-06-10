ed12894095b9475873d17fb3a5dc8f1407ebc240
tatarynov <vtatarynov@cogniance.com>
2017-06-10 08:47:57 +0200

    initial commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,28 @@
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class FirstTest {
+
+    private static AndroidDriver driver;
+
+    public static void main(String[] args) {
+        File classpathRoot = new File(System.getProperty("user.dir"));
+        File appDir = new File(classpathRoot, "/app/Android");
+        File app = new File(appDir, "Contacts.apk");
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "NotUsed");
+        capabilities.setCapability("app", app.getAbsolutePath());
+        capabilities.setCapability("appPackage", "com.jayway.contacts");
+        capabilities.setCapability("appActivity", "com.jayway.contacts.MainActivity");
+        try {
+             driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+}
