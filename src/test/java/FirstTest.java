814abf9230fd2233268db306de46420bc378b409
tatarynov <vtatarynov@cogniance.com>
2017-06-11 13:11:47 +0300

    Added steps with different locating strategies

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,9 +1,14 @@
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class FirstTest {
 
@@ -25,6 +30,29 @@ public class FirstTest {
             e.printStackTrace();
         }
 
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
+        MobileElement mainSearch = (MobileElement) driver.findElementById("main_search");
+        mainSearch.sendKeys("Joy");
+
+        MobileElement elementByXpath = (MobileElement) driver.findElementByXPath("//android.widget.TextView[@resource-id='com.jayway.contacts:id/name']");
+        elementByXpath.click();
+
+        String email = findElementById("email").getText();
+
+        WebDriverWait webDriverWait = new WebDriverWait(driver, 10);
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(By.id("email")));
+
+        System.out.println(email.equals("alstclair11@yopmail.com"));
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static MobileElement findElementById(String id) {
+        return (MobileElement) driver.findElementById(id);
     }
 }
