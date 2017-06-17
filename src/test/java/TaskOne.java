6cebe1d6c1448cffa169c46ff4f8c0b08adf0c58
tatarynov <vtatarynov@cogniance.com>
2017-06-17 19:03:20 +0300

    Lecture 5 - testng

---
+++ src/test/java/TaskOne.java
@@ -0,0 +1,28 @@
+public class TaskOne {
+
+    /***
+     * Create test class and use in it following annotations:
+
+     @Test
+     @BeforeClass
+     @AfterClass
+     @BeforeMethod
+     @AfterMethod
+
+
+     // Example
+
+     @BeforeClass
+     public void beforeClass() {
+        System.out.println("I will print something before class");
+     }
+
+     @AfterMethod
+     public void afterMethodPrint() {
+        System.out.println("I will printh something after method");
+     }
+     */
+
+
+
+}
