f28cd5e320b5fb5aaf144a73c4409af0fd16cff2
tatarynov <vtatarynov@cogniance.com>
2017-06-17 19:14:16 +0300

    Lecture 5: added parameters and asserts

---
+++ src/test/java/parameters/DataProviderTest.java
@@ -0,0 +1,25 @@
+package parameters;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class DataProviderTest {
+
+    @DataProvider
+    private Object[][] credentials() {
+        return new Object[][]{
+                {"user1@gmail.com", "Password11"},
+                {"user2@gmail.com", "Password22"},
+                {"user3@gmail.com", "Password33"},
+                {"user4@gmail.com", "Password44"}
+        };
+    }
+
+    @Test(dataProvider = "credentials")
+    public void login(String username, String password) {
+        System.out.println("Login with: " + username);
+        System.out.println("Password for this login: " + password);
+        System.out.println("");
+        System.out.println("");
+    }
+}
